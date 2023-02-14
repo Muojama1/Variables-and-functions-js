948b005e1461a0cee4810335bcf7e0c210cb5634
Muojama1 <sandramuojama@gmail.com>
2023-02-14 10:16:06 +0100

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+// function that multiplies two numbers
+function multiply (number1, number2) {
+    return number1 * number2;
+};
+console.log (multiply(78, 3));
+
+
+let firstname= Sandra ;
+let lastname= Muojama;
+let Stuterntrack= Mobile-development;
+let skills= UIdesign;
+let favouritecolor= Yellow;
+let Countrysindependenceyear= 1960;
+
+console.log ('My name is ' + firstname + lastname + '.I am currently learning ' + Stuterntrack + 'at Stutern, in addition to my other skill ' + skills + '.My best color is ' + favouritecolor + '.My country got Its independence in the year ' + Countrysindependenceyear);
